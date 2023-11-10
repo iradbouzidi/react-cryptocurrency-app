8fa7698cb11230b1ebd44cfd813d59e61a2824c1
iradbouzidi <b.irad1994@gmail.com>
2023-11-11 00:24:08 +0100

    layout done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "antd/dist/antd.css";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // https://www.youtube.com/watch?v=9DDX3US3kss
 // https://github1s.com/adrianhajdin/project_cryptoverse
