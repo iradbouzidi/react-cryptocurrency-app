0ee7cf956e233511dfae2fbfef2a6d79a59af24a
iradbouzidi <b.irad1994@gmail.com>
2023-11-10 23:39:22 +0100

    ready

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// https://www.youtube.com/watch?v=9DDX3US3kss
+// https://github1s.com/adrianhajdin/project_cryptoverse
