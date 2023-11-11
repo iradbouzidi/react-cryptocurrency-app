ca36d88a694970f3f2c20981f3b07918d828e417
iradbouzidi <b.irad1994@gmail.com>
2023-11-11 15:55:01 +0100

    cryptos done

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -11,7 +11,7 @@ import Loader from "./Loader";
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if (isFetching) return <Loader />;
